66a5fbe17df26bc9daa90451e4ee0fcfe1269b4e
loiaconof <francescoloiacono95@outlook.it>
2024-01-30 08:53:26 +0100

    feat: docs init

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,21 +8,32 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Docs', link: '/docs/getting-started/introduction' },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'Getting Started',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
+          { text: 'Introduction', link: '/docs/getting-started/introduction' },
+          { text: 'Quick start', link: '/docs/getting-started/quick-start' },
+        ],
+      },
+      {
+        text: 'Components',
+        items: [
+          {
+            text: 'Table',
+            items: [
+              { text: 'Introduction', link: '/docs/components/table/introduction' },
+            ],
+          },
+        ],
       }
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/loiaconof/vue-json-components' }
     ]
   }
 })
